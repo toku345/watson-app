19edc6971343377b211881d8792c5a6e9b0560b7
Fumitaka Tokumitsu <toku345@gmail.com>
2016-07-06 06:40:36 +0900

    hello world!

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject watson-app "0.0.1"
+  :description "watson bot sample"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [compojure "1.5.1"]])
---
+++ test/watson_app/core_test.clj
@@ -0,0 +1,7 @@
+(ns watson-app.core-test
+  (:require [clojure.test :refer :all]
+            [watson-app.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
